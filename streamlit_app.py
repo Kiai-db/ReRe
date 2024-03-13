b3c1d360ff19bc7e4177c2054f093e2bc2261d89
Kiai-db <alexanderkdbennett@outlook.com>
2024-03-13 15:26:14 +0000

    Alexs Subsystem

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,38 @@
 import streamlit as st # Import the rottenCNN function from rottendetector.py
-
+from Segment_Ingr import getobjects
+from classify_ingr import classify_ingr
+from PIL import Image
+import cv2
 def barcode_scanner_page():
     st.header("Renewable Recipes")
     # Add the content or functionality for this page
 
 
-# Define page content functions
 def veg_classifier_page():
     st.header("Vegetable Classifier")
-    # Add the content or functionality for this page
+
+    # Upload image from files
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Take a photo with the system's camera
+    camera_image = st.camera_input("Take a picture")
+
+    if uploaded_file is not None:
+        # Convert the uploaded file to an PIL Image
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        process_and_classify_image(image)
+    elif camera_image is not None:
+        # Convert the camera image to an PIL Image
+        image = Image.open(camera_image)
+        st.image(image, caption='Captured Image.', use_column_width=True)
+        process_and_classify_image(image)
+
+def process_and_classify_image(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+    pil_image_array = getobjects(image)
+    
+    ingredients = classify_ingr(pil_image_array)
+    st.write("Identified Ingredients:", ingredients)
 
 def rotten_classifier_page():
     st.header("Rotten Classifier")
@@ -35,17 +59,17 @@ def main():
 
     # Sidebar navigation
     st.sidebar.header("Main Navigation")
-    page = st.sidebar.radio("Go to", ("Veg Classifier", "Rotten Classifier", "Generate Recipe", "Barcode Scanner","Renewable Recipes"))
+    page = st.sidebar.radio("Go to", ("Alex: Veg Classifier", "Husain: Rotten Classifier", "James: Generate Recipe", "George: Barcode Scanner","Group: Renewable Recipes"))
 
-    if page == "Veg Classifier":
+    if page == "Alex: Veg Classifier":
         veg_classifier_page()
-    elif page == "Rotten Classifier":
+    elif page == "Husain: Rotten Classifier":
         rotten_classifier_page()
-    elif page == "Generate Recipe":
+    elif page == "James: Generate Recipe":
         generate_recipe_page()
-    elif page == "Barcode Scanner":
+    elif page == "George: Barcode Scanner":
         barcode_scanner_page()
-    elif page == "Renewable Recipes":
+    elif page == "Group: Renewable Recipes":
         rotten_classifier_page()
 
     # Main content section
