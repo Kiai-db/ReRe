33cb2b1400882b16f7a9aa3d66fe90539ea6d988
Husain-Ayoob <100984253+Husain-Ayoob@users.noreply.github.com>
2024-03-14 11:55:34 +0000

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,6 @@
 from Segment_Ingr import getobjects
 from classify_ingr import classify_ingr
-from generator_basic import generator
+from generator_basic import recipe_generator
 from rottendetector import rottenCNN
 from PIL import Image
 import cv2
@@ -17,10 +17,20 @@ ingredients = classify_ingr(pil_image_array)   #ingredient_i = (image, pred_clas
 
 
 #Husain
-rottenClass = []
+Class_Per_Ingr = []
+Class_Ingr = []
 for ingredient in ingredients:
     result = rottenCNN(ingredient(0))
-    rottenClass.append((result(0),result(1),ingredient(0)))
+    Class_Per_Ingr.append((result(0),result(1),ingredient(0)))
+    Class_Ingr.append((ingredient(0),result(0)))
+    if result(1) != "0%":
+        print(ingredient(0), " is ",result(1), " rotten. ")
+        if (result(1)(0)) > 50:
+            print("Bin the ingredient.")
+        else:
+            print("Ensure to remove the rotten part of the fruit, it is salvagable!")
+
+recipe = recipe_generator(Class_Ingr, "easy", "mexican")
 
 
-
+print(recipe)
